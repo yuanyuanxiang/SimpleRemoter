483d090515937975f8a27cccdfe34cbf781403b8
yuanyuanxiang <962914132@qq.com>
2025-01-14 23:55:37 +0800

    fix: #33 File gh0st2Remote.h can be merged

--- server/2015Remote/FileManagerDlg.h
+++ server/2015Remote/FileManagerDlg.h
@@ -4,7 +4,34 @@
 #if _MSC_VER > 1000
 #pragma once
 #endif // _MSC_VER > 1000
-#include "gh0st2Remote.h"
+
+#include "IOCPServer.h"
+
+#define CIOCPServer IOCPServer
+
+#define ClientContext CONTEXT_OBJECT
+
+typedef struct
+{
+	DWORD	dwSizeHigh;
+	DWORD	dwSizeLow;
+}FILESIZE;
+
+#define m_DeCompressionBuffer InDeCompressedBuffer
+
+#define GetBufferLen GetBufferLength
+
+#define m_Dialog hDlg
+
+#define m_Socket sClientSocket
+
+#define Send OnClientPreSending
+
+#define MAKEINT64(low, high) ((unsigned __int64)(((DWORD)(low)) | ((unsigned __int64)((DWORD)(high))) << 32))
+
+#define	MAX_WRITE_RETRY			15 // 重试写入文件次数
+#define	MAX_SEND_BUFFER			1024 * 8 // 最大发送数据长度
+#define MAX_RECV_BUFFER			1024 * 8 // 最大接收数据长度
 
 #define WM_MY_MESSAGE (WM_USER+300)
 
